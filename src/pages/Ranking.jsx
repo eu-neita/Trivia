43990969eebd590299054f9599bed46d4d45f302
keniapecanha <kenia.silvap@hotmail.com>
2023-04-20 19:59:44 -0300

    group-16-req-18

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Ranking extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  render() {
+    return (
+      <main>
+        <button
+          data-testid="btn-go-home"
+          onClick={ this.handleClick }
+        >
+          Restart
+        </button>
+      </main>
+    );
+  }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default Ranking;
